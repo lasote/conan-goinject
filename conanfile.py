aac68a0bb24d10998a8256928885526acfd52e47
Lasote <lasote@gmail.com>
2015-11-12 16:59:55 +0100

    Url parameter

--- conanfile.py
+++ conanfile.py
@@ -4,6 +4,7 @@ from conans import ConanFile
 class InjectConan(ConanFile):
     name = "go-inject"
     version = "1.0"
+    url = "https://github.com/lasote/conan-goinject"
 
     def source(self):
         self.run("git clone https://github.com/codegangsta/inject.git")
